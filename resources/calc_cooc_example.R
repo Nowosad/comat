4c22267c62ed4f07abe1390bdbab40aa54f05711
Nowosad <tupiszakaczki@gmail.com>
2019-05-25 13:47:01 +0200

    additional testing code

--- resources/calc_cooc_example.R
+++ resources/calc_cooc_example.R
@@ -9,46 +9,51 @@ require(rstudioapi) #use to automatically set working directory
 tryCatch(setwd(getSrcDirectory()[1]), error=function(e) setwd(dirname(rstudioapi::getActiveDocumentContext()$path)))
 
 
-##############
+############## 
 #census_sp contain 4 "layers" for Cook County
 ###cat - there is categorical raster with 5 classess (representing race categories 1- asian, 2-black, 3-hisp, 4-other, 5-white), use to identify pairs of categories in cooc matrix
 ###weights - there is numerical raster with weights used to calculate modified cooc matrix
 ###cns_units, cns_id - block ids - there are not used in this script
-census_sp = readRDS("data/sp_weights_adj_2010.rds")
+#census_sp = readRDS("data/sp_weights_adj_2010.rds")
 
-#shapefile use to crop raster
-region =st_read("data/c29_r30_60_60.shp", stringsAsFactors=FALSE)
+#shapefile use to crop raster 
+#region =st_read("data/c29_r30_60_60.shp", stringsAsFactors=FALSE)
 
 #prefix for results file
-fname = "c29_r30_60_adj_2010"
+fname = "example_"
 
-rcat = 1:5 # vector of categories assign to race
+rcat = 1:3 # vector of categories assign to race  
 
 #create raster stack based on census sp
-rast = stack(census_sp)
+#rast = stack(census_sp)
 
 #remove census sp - calculation are based on raster object provided by raster package
-rm(census_sp)
+#rm(census_sp)
 
 #crop input raster to shapefile region
-rst = crop(rast, region)
-rstm <- mask(rst, region)
+#rst = crop(rast, region)
+#rstm <- mask(rst, region)
 
-#r is a raster with categories
-r = rstm[["cat"]]
 
-#rw is raster with weights
-rw = rstm[["weights"]]
 
-clr = c("#f17160", "#6ebe44","#7e69af", "#c27515","#f8df1d") #colors for race categories
+#r is a raster with categories 
+r = raster(nrows = 3, ncols = 3,
+           xmn = 0, xmx = 3, ymn = 0, ymx = 3,
+           crs = NA,
+           vals = c(1, 1, 3, 1, 3, 3, 2, 2, 3))
+
+#rw is raster with weights 
+rw = raster(nrows = 3, ncols = 3,
+           xmn = 0, xmx = 3, ymn = 0, ymx = 3,
+           crs = NA,
+           vals = c(2, 2, 9, 6, 4, 9, 4, 8, 9))
+
+
+clr = c("yellow", "darkgreen", "red") #colors for race categories
 plot(r, col=clr)
 plot(rw)
 ##########################
 
-czas = Sys.time()
-# r = x
-# rw = w
-# rcat = 1:3
 #Create unique combination of race categories
 
 #unique combination
@@ -63,12 +68,12 @@ un2 = unique(sort_un[,1:2])
 un2$gr = 1:nrow(un2)
 sort_un2 = merge(sort_un, un2, by=c("V1", "V2"))
 
-#This data frame contain category for focus cell, neighboor cell and group (gr).
+#This data frame contain category for focus cell, neighboor cell and group (gr). 
 uniq = merge(un, sort_un2[, c("ord", "gr")], by="ord")
 colnames(uniq) = c("ord", "focus_cell", "neigh_cell", "gr")
 uniq$ord <- NULL
 #uniq
-
+uniq
 
 ptm <- proc.time()
 #result is a data frame with each pair focus-neigh cell
@@ -87,23 +92,23 @@ for (i in 1:nrow(r)) {
     res = as.data.frame(cbind(focus_cell , neigh_cell, w_focus_cell, w_neigh_cell, i, j))
     #print(res)
     #calculate value based on weights. In this example there is a mean. FUN can be any user-defined function
-    res$fnc = apply(res[, c("w_focus_cell", "w_neigh_cell")], 1, FUN=mean)
+    res$fnc = apply(res[, c("w_focus_cell", "w_neigh_cell")], 1, FUN=mean) 
     #res$fnc = w_focus_cell
     result = rbind(result, res)
 }#end ncol loop
-}#end nrow loop
+}#end nrow loop 
 
 
 #merge data frame result and uniq (contain 3 cols category of focus cell, neig cell and gr)
 result_df = merge(result, uniq, by = c("focus_cell", "neigh_cell"))
-# saveRDS(result_df, paste(fname, "_result_df.rds", sep=""))
+saveRDS(result_df, paste(fname, "_result_df.rds", sep=""))
 
-#just for check
+#just for check 
 table(result_df$gr)
 
 head(result_df)
 
-#Summarize result df by group
+#Summarize result df by group 
 #symetric matrix
 
 by_gr = group_by(result_df, by=gr)
@@ -123,15 +128,13 @@ proc.time() - ptm
 
 
 
-#display coocurence matrix based on out_df
+#display coocurence matrix based on out_df 
 
 make_mat <- function(x) {
-  x1=x[1:5]
-  x2=c(0, x[6:9])
-  x3=c(0, 0, x[10:12])
-  x4=c(0,0, 0, x[13:14])
-  x5=c(0, 0, 0, 0, x[15])
-  m = rbind(x1,x2,x3,x4,x5)
+  x1=x[1:3]
+  x2=c(0, x[4:5])
+  x3=c(0, 0, x[6])
+  m = rbind(x1,x2,x3)
   m[upper.tri(m,diag=FALSE)] <- m[upper.tri(m,diag=FALSE)]/2 #upper part without diagonal need to be divided by 2. #dodalam unlist jak nie zadziala
   m[lower.tri(m)] <- t(m)[lower.tri(m)] #copy value from upper part to lower part
   return(m)
@@ -144,21 +147,8 @@ mc
 mp = make_mat(out_df$suma)
 mp
 
-Sys.time() - czas
-
-rcpp_get_coocurrence_matrix(as.matrix(r), as.matrix(4))
-
-bench::mark(rcpp_get_wecoma(as.matrix(r), as.matrix(rw), matrix(4)))
-
-a = matrix(sample(1:5, size = 14000 * 14000, replace = TRUE), ncol = 14000, nrow = 14000)
-b = matrix(runif(14000 * 14000), ncol = 14000, nrow = 14000)
-
-
-bench::mark({d = rcpp_get_wecoma(a, b, matrix(4))})
-bench::mark({e = rcpp_get_wecoma(a, b, matrix(4), 2)})
-
-
-cor(as.vector(mp),
-    as.vector(rcpp_get_wecoma(as.matrix(r), as.matrix(rw), matrix(4))))
-
-# mp/rcpp_get_wecoma(as.matrix(r), as.matrix(rw), matrix(4))
+#Coocurence matrix dla przykładu 
+[,1] [,2] [,3]
+x1 12.0  5.0 13.5
+x2  5.0 12.0 14.5
+x3 13.5 14.5 49.0

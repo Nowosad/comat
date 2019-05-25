64bc7915271f9c294562cd26c5bccb215bd2435a
Nowosad <tupiszakaczki@gmail.com>
2019-05-25 13:46:50 +0200

    adds mroe test datasets

--- data-raw/01-example_data.R
+++ data-raw/01-example_data.R
@@ -10,6 +10,18 @@ w = raster(nrows = 3, ncols = 3,
            crs = NA,
            vals = c(2, 2, 9, 6, 4, 9, 4, 8, 9))
 
+x_na = raster(nrows = 3, ncols = 3,
+           xmn = 0, xmx = 3, ymn = 0, ymx = 3,
+           crs = NA,
+           vals = c(1, 1, 3, NA, 3, NA, 2, 2, NA))
+
+w_na = raster(nrows = 3, ncols = 3,
+           xmn = 0, xmx = 3, ymn = 0, ymx = 3,
+           crs = NA,
+           vals = c(2, 2, NA, 6, NA, 9, 4, NA, 9))
+
 # rcpp_get_coocurrence_matrix(as.matrix(x), matrix(4))
 writeRaster(x, "data-raw/x.tif")
 writeRaster(w, "data-raw/w.tif")
+writeRaster(x_na, "data-raw/x_na.tif")
+writeRaster(w_na, "data-raw/w_na.tif")

98895fa088e57902a2328653936f1213d4c861ec
Nowosad <tupiszakaczki@gmail.com>
2019-06-19 19:37:31 +0200

    fixes header

--- src/rcpp_get_wecove.h
+++ src/rcpp_get_wecove.h
@@ -6,11 +6,7 @@
 #include <RcppArmadillo.h>
 using namespace Rcpp;
 
-Rcpp::NumericVector rcpp_get_wecove(const Rcpp::IntegerMatrix x,
-                              const Rcpp::NumericMatrix w,
-                              const arma::imat directions,
-                              const std::string fun = "mean",
-                              const std::string na_action = "replace",
-                              bool ordered = TRUE);
+Rcpp::NumericVector rcpp_get_wecove(const Rcpp::NumericMatrix x,
+                                    bool ordered);
 
 #endif // RCPP_GET_WECOVE_H

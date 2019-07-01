8ece96a0dbb2c8de848e32efa4bd46132d3c98dd
jn <tupiszakaczki@gmail.com>
2019-07-01 17:43:26 +0200

    adds default values

--- R/get_wecoma.R
+++ R/get_wecoma.R
@@ -4,13 +4,13 @@
 #' @param w A RasterStack
 #' @param neighbourhood The number of directions in which cell adjacencies are considered as neighbours:
 #' 4 (rook's case) or 8 (queen's case). The default is 4.
-#' @param fun "mean", "geometric_mean", or "focal"
-#' @param na_action "replace", "omit", or "keep"
+#' @param fun "mean", "geometric_mean", or "focal". The default is "mean".
+#' @param na_action "replace", "omit", or "keep". The default is "replace".
 #'
 #' @return A RasterStack
 #' @export
 #'
-get_wecoma = function(x, w, neighbourhood, fun, na_action){
+get_wecoma = function(x, w, neighbourhood = 4, fun = "mean", na_action = "replace"){
   x = raster::as.matrix(x)
   w = raster::as.matrix(w)
   directions = as.matrix(neighbourhood)

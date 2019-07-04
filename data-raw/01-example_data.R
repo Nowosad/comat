d7617f30cbfffc40baa15d849442953179d506fc
jn <tupiszakaczki@gmail.com>
2019-07-04 14:35:54 +0200

    adds example data

--- data-raw/01-example_data.R
+++ data-raw/01-example_data.R
@@ -25,3 +25,9 @@ writeRaster(x, "data-raw/x.tif")
 writeRaster(w, "data-raw/w.tif")
 writeRaster(x_na, "data-raw/x_na.tif")
 writeRaster(w_na, "data-raw/w_na.tif")
+
+usethis::use_data(x)
+usethis::use_data(w)
+usethis::use_data(x_na)
+usethis::use_data(w_na)
+

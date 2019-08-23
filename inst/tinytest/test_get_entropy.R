0ddcfa181254cd8db3485877bdf2778b9e7face4
nowosad <dukekubas@gmail.com>
2019-08-23 12:27:46 +0200

    adds entropy tests

---
+++ inst/tinytest/test_get_entropy.R
@@ -0,0 +1,11 @@
+r1 = rcpp_get_entropy(c(5, 9), "log2")
+r2 = rcpp_get_entropy(c(5, 9), "log")
+
+buys = c("no", "no", "yes", "yes", "yes", "no", "yes", "no", "yes", "yes", "yes", "yes", "yes", "no")
+freqs = table(buys)/length(buys)
+
+t1 = -sum(freqs * log2(freqs))
+t2 = -sum(freqs * log(freqs))
+
+expect_equal(r1, t1)
+expect_equal(r2, t2)

116ffb3aff603dad8f5cc992178b9cbac7fe30e8
nowosad <dukekubas@gmail.com>
2019-08-23 12:52:55 +0200

    specify functions

--- inst/tinytest/test_get_unique_values.R
+++ inst/tinytest/test_get_unique_values.R
@@ -1,4 +1,4 @@
 vector_x = c(1, 1, 1, 2, 1, 2, 2, NA)
 
-expect_equal(get_unique_values(vector_x, na_omit = TRUE), c(1, 2))
-expect_equal(get_unique_values(vector_x, na_omit = FALSE), c(NA, 1, 2))
+expect_equal(comat::get_unique_values(vector_x, na_omit = TRUE), c(1, 2))
+expect_equal(comat::get_unique_values(vector_x, na_omit = FALSE), c(NA, 1, 2))
--- inst/tinytest/test_triangular_index.R
+++ inst/tinytest/test_triangular_index.R
@@ -1,2 +1,2 @@
-expect_equal(triangular_index(0, 0), 0)
-expect_equal(triangular_index(1, 0), 1)
+expect_equal(comat::triangular_index(0, 0), 0)
+expect_equal(comat::triangular_index(1, 0), 1)

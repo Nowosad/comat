cbf69cf930ebc67b95c884a060430b0d6eaced81
nowosad <dukekubas@gmail.com>
2019-10-01 20:23:29 +0200

    adds r code for comparision

--- src/get_normalized.cpp
+++ src/get_normalized.cpp
@@ -2,7 +2,7 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericVector get_normalized(NumericVector x, std::string normalization) {
+NumericVector get_normalized(NumericVector &x, std::string normalization) {
   unsigned int len_x = x.length();
   NumericVector y(len_x);
   double sum_x = sum(x);
@@ -18,4 +18,16 @@ NumericVector get_normalized(NumericVector x, std::string normalization) {
 
 /*** R
 get_normalized(c(3, 4.5, 2, 1, 0.2), normalization = "pdf")
+# get_normalized2 = function(x, normalization){
+#   sum_x = sum(x)
+#   if (sum_x == 0){
+#     return(rep(0, length(x)))
+#   } else if (normalization == "pdf"){
+#     return(x/sum_x)
+#   }
+# }
+# bench::mark(
+#   get_normalized(c(3, 4.5, 2, 1, 0.2), normalization = "pdf"),
+#   get_normalized2(c(3, 4.5, 2, 1, 0.2), normalization = "pdf")
+# )
 */

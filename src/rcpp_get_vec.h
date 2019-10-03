7b1b7955b73af61345a2c6dda2ce5370264b1571
nowosad <dukekubas@gmail.com>
2019-10-03 19:56:41 +0200

    adds new headers

--- src/rcpp_get_vec.h
+++ src/rcpp_get_vec.h
@@ -2,6 +2,7 @@
 #define GET_VEC_H
 #include <RcppArmadillo.h>
 #include "triangular_index.h"
+#include "get_unique_values.h"
 #include "get_normalized.h"
 using namespace Rcpp;
 
@@ -10,6 +11,16 @@ using namespace Rcpp;
 
 NumericVector rcpp_get_vec(NumericMatrix x, bool ordered = true,
                            std::string normalization = "none");
-NumericVector rcpp_get_wecove(NumericMatrix x, bool ordered = true);
 
+NumericVector rcpp_get_wecove(NumericMatrix x, std::string type = "ordered",
+                              std::string normalization = "none");
+
+NumericVector rcpp_get_cove(IntegerMatrix x, std::string type = "ordered",
+                              std::string normalization = "none");
+
+NumericVector rcpp_get_cocove(IntegerMatrix x, std::string type = "ordered",
+                              std::string normalization = "none");
+
+NumericVector rcpp_get_incove(List x, std::string type = "ordered",
+                              std::string normalization = "none");
 #endif // GET_VEC_H

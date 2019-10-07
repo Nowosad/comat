2d7bf4d5e8357217aafc8e9e55b13ede943b5170
nowosad <dukekubas@gmail.com>
2019-10-07 20:23:13 +0200

    tests tiny impr

--- R/comat-data.R
+++ R/comat-data.R
@@ -1,19 +1,19 @@
 #' A matrix with categories
-#'
+#' @usage data(raster_x)
 #' @format A matrix
 "raster_x"
 
 #' A matrix with weights
-#'
+#' @usage data(raster_w)
 #' @format A matrix
 "raster_w"
 
 #' A matrix with categories and missing values
-#'
+#' @usage data(raster_x_na)
 #' @format A matrix
 "raster_x_na"
 
 #' A matrix with weights and missing values
-#'
+#' @usage data(raster_w_na)
 #' @format A matrix
 "raster_w_na"

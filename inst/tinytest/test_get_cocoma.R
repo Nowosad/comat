2768047472f7abcfee692ebfc2db2c4853b253ad
nowosad <dukekubas@gmail.com>
2019-10-08 09:27:36 +0200

    adds cocoma tests

---
+++ inst/tinytest/test_get_cocoma.R
@@ -0,0 +1,15 @@
+data(raster_x, package = "comat")
+data(raster_w, package = "comat")
+
+r1 = get_cocoma(raster_x, raster_w, neighbourhood = 4)
+
+t1 = structure(c(3L, 0L, 2L, 3L, 2L, 1L, 1L, 1L, 1L, 0L, 1L, 2L, 1L, 1L, 5L),
+               .Dim = c(3L, 5L),
+               .Dimnames = list(c("1", "2", "3"), c("2", "4", "6", "8", "9")))
+
+expect_equivalent(r1, t1)
+
+r2 = get_cocove(r1, ordered = TRUE, normalization = "none")
+t2 = c(3, 0, 2, 3, 2, 1, 1, 1, 1, 0, 1, 2, 1, 1, 5)
+expect_equivalent(r2, t2)
+

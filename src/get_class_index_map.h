e4cbc27d078acbf64ac7be7ee6ed53fbacf0e522
Nowosad <tupiszakaczki@gmail.com>
2020-01-17 12:25:51 +0100

    exports get_glass_index_map

--- src/get_class_index_map.h
+++ src/get_class_index_map.h
@@ -2,6 +2,7 @@
 #define GET_CLASS_INDEX_MAP_H
 #include <vector>
 #include <map>
+//[[Rcpp::interfaces(r, cpp)]]
 
 std::map<int, unsigned> get_class_index_map(const std::vector<int> &classes);
 #endif // GET_CLASS_INDEX_MAP_H

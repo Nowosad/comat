11a5c6af4bcc8a5a1f1930165458b14af9bc7037
nowosad <dukekubas@gmail.com>
2020-03-21 12:51:55 +0100

    adds tiny impr to the docs

--- R/get_wecoma.R
+++ R/get_wecoma.R
@@ -6,7 +6,7 @@
 #' 4 (rook's case) or 8 (queen's case). The default is 4.
 #' @param classes A vector or a list with the values of selected classes from the `x` object.
 #' It is used to calculate wecoma only for selected classes.
-#' @param fun Function to calculate values from adjacent cells to contribute to exposure matrix, `"mean"` - calculate average values from adjacent cells of weight matrix, `"geometric_mean"` - calculate geometric mean values from adjacent cells of weight matrix, or `"focal"` assign value from the focal cell.
+#' @param fun Function to calculate values from adjacent cells to contribute to output matrix, `"mean"` - calculate average values from adjacent cells of weight matrix, `"geometric_mean"` - calculate geometric mean values from adjacent cells of weight matrix, or `"focal"` assign value from the focal cell.
 #' @param na_action Decides on how to behave in the presence of missing values in `w`. Possible options are `"replace"`, `"omit"`, `"keep"`. The default, `"replace"`, replaces missing values with 0, `"omit"` does not use cells with missing values, and `"keep"` keeps missing values.
 #'
 #' @return A weighted co-occurrence matrix
